edc4e5e498dfda0cf2e4ab83f4f113bf897d9b6a
RuneRogue <priyankmakwana305@gmail.com>
2025-03-26 22:37:48 +0530

    format code

--- src/TaskManager.sol
+++ src/TaskManager.sol
@@ -1,65 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-// As multiple user can have multiple task so We will not be using Ownable from openzeppline because 
+// As multiple user can have multiple task so We will not be using Ownable from openzeppline because
 // different Task may have different owners.
 contract TaskManager {
-    struct Task{
+    struct Task {
         uint256 id;
         string title;
         string description;
         bool completed;
         address owner;
     }
-    //As multiple user will have multiple task so we'll be using mapping of address and task array. 
+    //As multiple user will have multiple task so we'll be using mapping of address and task array.
     //Index of Task array can be considered as taskId(i.e Index+1 , starts from 1)
-    mapping (address=>Task[]) private tasks;
+
+    mapping(address => Task[]) private tasks;
 
     event TaskCreated(address indexed user, uint256 taskId, string title);
     event TaskCompleted(address indexed user, uint256 taskId);
     event TaskEdited(address indexed user, uint256 taskId, string newTitle, string newDescription);
     event TaskDeleted(address indexed user, uint256 taskId);
 
-    modifier onlyTaskOwner(uint _id){
-        require(_id>0 && _id <=tasks[msg.sender].length,"Invalid Task Id");
-        require(tasks[msg.sender][_id-1].owner==msg.sender,"You are not owner of task");
+    modifier onlyTaskOwner(uint256 _id) {
+        require(_id > 0 && _id <= tasks[msg.sender].length, "Invalid Task Id");
+        require(tasks[msg.sender][_id - 1].owner == msg.sender, "You are not owner of task");
         _;
     }
 
-    function createNewTask(string memory _title, string memory _description ) public {
-        require(bytes(_title).length>0,"Empty title");
-        uint task_id = tasks[msg.sender].length+1;
-        Task memory task = Task(task_id,_title,_description,false, msg.sender);
+    function createNewTask(string memory _title, string memory _description) public {
+        require(bytes(_title).length > 0, "Empty title");
+        uint256 task_id = tasks[msg.sender].length + 1;
+        Task memory task = Task(task_id, _title, _description, false, msg.sender);
         tasks[msg.sender].push(task);
         emit TaskCreated(msg.sender, task_id, _title);
     }
 
-    function completeTask(uint _id) public onlyTaskOwner(_id) {
-        tasks[msg.sender][_id-1].completed = !tasks[msg.sender][_id-1].completed;
+    function completeTask(uint256 _id) public onlyTaskOwner(_id) {
+        tasks[msg.sender][_id - 1].completed = !tasks[msg.sender][_id - 1].completed;
         emit TaskCompleted(msg.sender, _id);
     }
 
-    function editTask(uint _id , string memory newTitle,string memory newDescription) public onlyTaskOwner(_id) {
-        require(bytes(newTitle).length>0,"Empty title");
-        tasks[msg.sender][_id-1].title = newTitle;
-        tasks[msg.sender][_id-1].description = newDescription;
-       emit TaskEdited(msg.sender, _id , newTitle, newDescription);
+    function editTask(uint256 _id, string memory newTitle, string memory newDescription) public onlyTaskOwner(_id) {
+        require(bytes(newTitle).length > 0, "Empty title");
+        tasks[msg.sender][_id - 1].title = newTitle;
+        tasks[msg.sender][_id - 1].description = newDescription;
+        emit TaskEdited(msg.sender, _id, newTitle, newDescription);
     }
 
-    function deleteTask(uint _id) public onlyTaskOwner(_id) {
-        for (uint i=_id-1; i<tasks[msg.sender].length-1 ; i++) 
-        {
-            tasks[msg.sender][i] = tasks[msg.sender][i+1];
+    function deleteTask(uint256 _id) public onlyTaskOwner(_id) {
+        for (uint256 i = _id - 1; i < tasks[msg.sender].length - 1; i++) {
+            tasks[msg.sender][i] = tasks[msg.sender][i + 1];
         }
         tasks[msg.sender].pop();
-        emit TaskDeleted(msg.sender,_id);
+        emit TaskDeleted(msg.sender, _id);
     }
 
-    function getTask(uint _id)public view onlyTaskOwner(_id) returns (string memory, string memory, bool) {
-        return (tasks[msg.sender][_id-1].title,tasks[msg.sender][_id-1].description ,tasks[msg.sender][_id-1].completed);
+    function getTask(uint256 _id) public view onlyTaskOwner(_id) returns (string memory, string memory, bool) {
+        return (
+            tasks[msg.sender][_id - 1].title,
+            tasks[msg.sender][_id - 1].description,
+            tasks[msg.sender][_id - 1].completed
+        );
     }
 
-    function getAllTask()public view returns (Task[] memory) {
+    function getAllTask() public view returns (Task[] memory) {
         return tasks[msg.sender];
     }
 }
--- test/TaskManager.t.sol
+++ test/TaskManager.t.sol
@@ -17,6 +17,7 @@ contract TaskManagerTest is Test {
         task_manager = script.run();
     }
     //Checking whether task is created or not , Also checking whether each user have it's own task array or not.
+
     function test_createTask() public {
         vm.prank(user1);
         task_manager.createNewTask("Task1 of user1", "Testing Task1");
@@ -41,7 +42,7 @@ contract TaskManagerTest is Test {
         vm.prank(user1);
         task_manager.completeTask(1);
         vm.prank(user1);
-        (, , bool completed) = task_manager.getTask(1);
+        (,, bool completed) = task_manager.getTask(1);
         assertEq(completed, true);
     }
 
@@ -59,9 +60,9 @@ contract TaskManagerTest is Test {
 
     function test_DeleteTask() public {
         vm.prank(user1);
-        task_manager.createNewTask("Task1 of user1","Testing Task1");
+        task_manager.createNewTask("Task1 of user1", "Testing Task1");
         vm.prank(user1);
-        task_manager.createNewTask("Task2 of user1","Testing Task2");
+        task_manager.createNewTask("Task2 of user1", "Testing Task2");
         vm.prank(user1);
         task_manager.deleteTask(1);
         vm.prank(user1);
@@ -76,15 +77,15 @@ contract TaskManagerTest is Test {
         task_manager.getTask(1);
     }
 
-    function test_Access() public{
+    function test_Access() public {
         vm.prank(user1);
-        task_manager.createNewTask("Task1 of user1","Testing Task1");
+        task_manager.createNewTask("Task1 of user1", "Testing Task1");
         vm.expectRevert("Invalid Task Id");
         vm.prank(user2);
         task_manager.completeTask(1);
         vm.expectRevert("Invalid Task Id");
         vm.prank(user2);
-        task_manager.editTask(1,"Task1 of user1 edited","Testing Task1 edited");
+        task_manager.editTask(1, "Task1 of user1 edited", "Testing Task1 edited");
         vm.expectRevert("Invalid Task Id");
         vm.prank(user2);
         task_manager.deleteTask(1);
@@ -93,31 +94,30 @@ contract TaskManagerTest is Test {
         task_manager.getTask(1);
     }
 
-    function test_invalid_id() public{
+    function test_invalid_id() public {
         vm.expectRevert("Invalid Task Id");
         vm.prank(user1);
         task_manager.deleteTask(0);
     }
 
-    function test_empty_title() public{
+    function test_empty_title() public {
         vm.expectRevert("Empty title");
         vm.prank(user1);
-        task_manager.createNewTask("","Testing Task1");
+        task_manager.createNewTask("", "Testing Task1");
     }
 
-    function test_getAllTask() public{
+    function test_getAllTask() public {
         vm.prank(user1);
-        task_manager.createNewTask("Task1 of user1","Testing Task1");
+        task_manager.createNewTask("Task1 of user1", "Testing Task1");
         vm.prank(user1);
-        task_manager.createNewTask("Task2 of user1","Testing Task2");
+        task_manager.createNewTask("Task2 of user1", "Testing Task2");
         vm.prank(user1);
-        task_manager.createNewTask("Task3 of user1","Testing Task3");
+        task_manager.createNewTask("Task3 of user1", "Testing Task3");
         vm.prank(user1);
         TaskManager.Task[] memory task = task_manager.getAllTask();
-        assertEq(task.length,3);
-        assertEq(task[0].id,1);
-        assertEq(task[1].id,2);
-        assertEq(task[2].id,3);
+        assertEq(task.length, 3);
+        assertEq(task[0].id, 1);
+        assertEq(task[1].id, 2);
+        assertEq(task[2].id, 3);
     }
-
 }
